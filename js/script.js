ecee1bf0cba1466dad9c28375fc2cf83bd16a9a5
Derek Tran <88995035+datderek@users.noreply.github.com>
2023-06-04 16:45:34 -0700

    Implement basic arithmetic operations

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let firstNum;
+let secondNum;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
