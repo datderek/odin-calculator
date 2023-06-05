76e53ffecb75e00d24f30000a20563b990adaa4a
Derek Tran <88995035+datderek@users.noreply.github.com>
2023-06-04 17:31:51 -0700

    Create ugly buttons

--- js/script.js
+++ js/script.js
@@ -2,6 +2,23 @@ let firstNum;
 let secondNum;
 let operator;
 
+function operate(firstNum, secondNum, operation) {
+    switch(operation) {
+        case "+":
+            add(firstNum, secondNum);
+            break;
+        case "-":
+            subtract(firstNum, secondNum);
+            break;
+        case "*":
+            multiply(firstNum, secondNum);
+            break;
+        case "/":
+            divide(firstNum, secondNum);
+            break;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
