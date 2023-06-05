73880f4aa192367e663bd3adeeceeb201f25e346
Derek Tran <88995035+datderek@users.noreply.github.com>
2023-06-04 20:43:05 -0700

    Update display on user input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = "";
+let secondNum = "";
+let operator = null;
+let numSwitcher = false;
+
+let numbers = document.querySelectorAll('#numbers');
+numbers.forEach(number => {
+    number.addEventListener('click', e => handleInput(e.target.parentNode.id, e.target.id));
+});
+
+let operators = document.querySelectorAll('#operators');
+operators.forEach(operator => {
+    operator.addEventListener('click', e => handleInput(e.target.parentNode.id, e.target.id));
+});
+
+function handleInput(type, item) {
+    if (type === 'numbers') {
+        if (numSwitcher) {
+            secondNum += item;
+        } else {
+            firstNum += item;
+        }
+    } else if (type === 'operators' && firstNum) {
+        operator = item;
+        numSwitcher = true;
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    let display = document.querySelectorAll('#display > *');
+    display[0].textContent = firstNum;
+    display[1].textContent = operator;
+    display[2].textContent = secondNum;
+}
 
 function operate(firstNum, secondNum, operation) {
     switch(operation) {
